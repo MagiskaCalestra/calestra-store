8cc65cc5328dbcbbb222f5ab7b7fbc8a083c6dc7
MagiskaCalestra <magiskacalestra@gmail.com>
2025-07-13 12:28:25 +0300

    Första uppladdning

---
+++ index.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Drop01Page from "./Drop01Page";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Drop01Page />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
