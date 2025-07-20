230c3d5f4276344b67a6addf1c4d5b70becfdfe5
MagiskaCalestra <magiskacalestra@gmail.com>
2025-07-20 15:05:55 +0300

    Add files via upload

---
+++ page/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Välkommen till Calestra Store</h1>
+      <p>Butiken är under uppbyggnad.</p>
+    </div>
+  )
+}
