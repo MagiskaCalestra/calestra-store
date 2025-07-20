522ab96926190dbb2327c113e10fdaaf4a9ba50e
MagiskaCalestra <magiskacalestra@gmail.com>
2025-07-20 16:06:38 +0300

    Delete page directory

--- page/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-      <h1>Välkommen till Calestra Store</h1>
-      <p>Butiken är under uppbyggnad.</p>
-    </div>
-  )
-}
