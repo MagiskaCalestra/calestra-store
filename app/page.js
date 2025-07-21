3f79ca797db92d50812aa9f36160c4c04e9c754c
MagiskaCalestra <magiskacalestra@gmail.com>
2025-07-21 12:08:29 +0300

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif", textAlign: "center" }}>
-      <h1>Välkommen till Calestra Store!</h1>
-      <p>✨ Din magiska butik för känslor, stil och fantasi ✨</p>
-    </main>
-  );
-}
