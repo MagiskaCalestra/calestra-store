4b3fab39409e40769f30d1f3d65c1a45822540a0
MagiskaCalestra <magiskacalestra@gmail.com>
2025-07-21 12:11:35 +0300

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-// lib/api.js
-
-export async function fetchData(url) {
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error('Nätverksfel vid hämtning av data');
-  }
-  return res.json();
-}
